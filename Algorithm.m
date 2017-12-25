90274bb00383d7790077021342a8d19263d5aab9
mohammadreza <mr_asadi66@yahoo.com>
2017-12-25 17:08:09 +0330

    successfully break alpha pinene benchmark in 41 second

--- Algorithm.m
+++ Algorithm.m
@@ -4,39 +4,60 @@ filename = 'main_1.txt';
 data=csvread(filename);
 expt=data(:,1);
 expx=data(1:8,2:6);
-pop=100;best=10;sub_range=4;param_count=5;f_rebuild=10^-1*ones(1,6);
+pop=100;best=10;sub_range=4;param_count=5;f_rebuild=20;
+total = 41; pop_rebuild = 10;
 Population = Log_Init(pop,sub_range,param_count);
 Population = Simulate_Second(Population,expt,expx);
 Population = sortrows(Population,6);
+Best = Population(1,6);
 Ref_Set = Ref_Set_Generation_First(Population,best);
-
 b = size(Ref_Set,1);
-% t = timer('TimerFcn', 'stat = false;/','StartDelay',1200);
-% start(t)
-% stat = true;
-total=5;
-tot=5;
 result_curve = zeros(total,1);
-% while(stat == true)
+result_curve(1,:) = Best;
+noImprovement = 0;
+noimp_cnt=0;
+rebuild_cnt=0;
+counter=1;
+t = timer('TimerFcn', 'stat=false;','StartDelay',total);
+start(t);
+stat=true;
 tic;
-for counter = 1:total
-%     counter = counter+1;
+while(stat==true)
+% 
+% for counter = 1:total
+    counter = counter+1;
 
 % check for standard deviation of ref_set values for preventing stuck in
 % flat regions and adding new diverse vector by distance filter
-%     f_reb = std(Ref_Set);
-%     if(f_reb > f_rebuild)
+    if(noImprovement > pop_rebuild)
+        noImprovement = 0;
+        Population = Log_Init(pop/4,sub_range,param_count);
+        Population = Simulate_Second(Population,expt,expx);
+        Population = sortrows(Population,6);
+        Ref_Temp = Ref_Set_Generation_First(Population,best);
+        Ref_Temp(1,:) = Ref_Set(1,:);
+        Ref_Set = Ref_Temp;
+        noimp_cnt = noimp_cnt + 1;
+    end
+    f_reb = sum(std(Ref_Set(:,6)));
+    if(f_reb > f_rebuild)
         Combinations = Combination(Ref_Set,b);
         simulated = Simulate_Second(Combinations,expt,expx);
         simulated = sortrows(simulated,6);
         Ref_Set = Ref_Set_Update(Ref_Set,simulated);
-        result_curve(counter,:) = Ref_Set(1,6);
-%     else
-%         Ref_Set_Temp = Ref_Set_Generation_Second(Population,Ref_Set);
-%         Ref_Set=Rebuild_Ref_Set(Ref_Set,Ref_Set_Temp);
-%     end
+    else
+        rebuild_cnt = rebuild_cnt + 1;
+        noImprovement = noImprovement + 1;
+        Ref_Set_Temp=Ref_Set_Generation_First(Population,best);
+        Ref_Set = Rebuild_Ref_Set(Ref_Set,Ref_Set_Temp);
+    end
+    if(Ref_Set(1,6) < Best)
+       Best = Ref_Set(1,6);
+    else
+        noImprovement = noImprovement + 1;
+    end
+    result_curve(counter,:) = Best;
 end
-
 toc;
 plot(result_curve(:,1));
 tspan = 0:1:40000;
@@ -52,4 +73,5 @@ testX(testX==0) = nan ;
 figure;
 plot(tt,y,tt,testX,'.');
 Best=Ref_Set(1,6);
-disp(Best);
+str=strcat("best is : ",num2str(Best)," rebuild_cnt=: ",num2str(rebuild_cnt)," noimp_cnt: ",num2str(noimp_cnt));
+disp(str);
--- Rebuild_Ref_Set.m
+++ Rebuild_Ref_Set.m
@@ -1,8 +1,9 @@
 function [ Ref_Set ] = Rebuild_Ref_Set( Ref_Set,Population )
 ref_count = size(Ref_Set,1);
 for i = ref_count/2:ref_count
-    index = i - ref_count/2 +1; %Distance_Factor(Population,Ref_Set(i,:));
+    index = Distance_Factor(Population,Ref_Set(i,:));
     Ref_Set(i,:)=Population(index,:);
+    Population(index,:)=[];
 end
 end
 
